daad0e2edbc2e723eda2980858ded53d00c1fbb1
Ilya Semyonov <ilya.semyonov.03@mail.ru>
2023-05-24 21:36:35 +0300

    add: move task func

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -5,6 +5,7 @@ import {useAppDispatch, useAppSelector} from "../hooks/hooks";
 import {deleteTask, updateTask} from "../redux/slices/projectsSlice";
 import EditTaskModal from "./EditTaskModal";
 import {Task} from "../interfaces/Task";
+import MoveTaskModal from "./MoveTaskModal";
 
 interface ITaskProps {
     task: Task
@@ -12,7 +13,8 @@ interface ITaskProps {
 
 const TaskCard = ({task}: ITaskProps) => {
     const dispatch = useAppDispatch()
-    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [modalEditIsOpen, setModalEditIsOpen] = useState(false);
+    const [modalMoveTaskIsOpen, setModalMoveTaskIsOpen] = useState(false);
     const {projectId, sprintId} = useParams();
     const project = useAppSelector(state => state.project.find(project => project.id === projectId))
 
@@ -20,7 +22,7 @@ const TaskCard = ({task}: ITaskProps) => {
         dispatch(deleteTask(task.id))
     }
     const handleCloseMoveTaskModal = () => {
-        setModalIsOpen(false)
+        setModalMoveTaskIsOpen(false)
     }
 
     const handleEditProjectSubmit = (task: Task) => {
@@ -29,8 +31,9 @@ const TaskCard = ({task}: ITaskProps) => {
 
     return (
         <>
-            <EditTaskModal open={modalIsOpen} task={task} handleCloseTaskModal={handleCloseMoveTaskModal}
+            <EditTaskModal open={modalEditIsOpen} task={task} handleCloseTaskModal={handleCloseMoveTaskModal}
                            onSubmit={handleEditProjectSubmit}/>
+            <MoveTaskModal taskId={task.id} project={project} sprintId={sprintId} handleCloseMoveTaskModal={handleCloseMoveTaskModal} open={modalMoveTaskIsOpen}/>
             <Card>
                 <CardActionArea>
                     <CardContent>
@@ -48,7 +51,8 @@ const TaskCard = ({task}: ITaskProps) => {
                         </Typography>
                     </CardContent>
                 </CardActionArea>
-                <Button onClick={()=>{setModalIsOpen(true)}}>Редактировать</Button>
+                <Button onClick={()=>{setModalMoveTaskIsOpen(true)}}>Переместить</Button>
+                <Button onClick={()=>{setModalEditIsOpen(true)}}>Редактировать</Button>
                 <Button onClick={handleDeleteTask}>Удалить</Button>
             </Card>
         </>
