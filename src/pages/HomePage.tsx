3425d3f64da630087565589f76ed81cee26a895c
Ilya Semyonov <ilya.semyonov.03@mail.ru>
2023-05-24 21:06:54 +0300

    refactor: change redux func

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,9 @@
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Container, Grid, TextField} from "@mui/material";
 import ProjectForm, {IFormValues} from "../components/ProjectForm";
 import {addProject} from "../redux/slices/projectsSlice";
 import {useAppDispatch, useAppSelector} from "../hooks/hooks";
 import ProjectCard from "../components/ProjectCard";
-import {Link} from "react-router-dom";
 
 
 function HomePage() {
@@ -13,10 +12,9 @@ function HomePage() {
     const [searchTerm, setSearchTerm] = useState('');
     const [filteredProjects, setFilteredProjects] = useState(project);
 
-    useEffect(()=>{
+    useEffect(() => {
         setFilteredProjects(project.filter(project => project.id.toString().includes(searchTerm) || project.title.toLowerCase().includes(searchTerm.toLowerCase())));
-    },[project])
-
+    }, [project])
 
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
@@ -27,14 +25,14 @@ function HomePage() {
         setFilteredProjects(project.filter(project => project.id.toString().includes(searchTerm) || project.title.toLowerCase().includes(searchTerm.toLowerCase())));
     };
 
-    const handleProjectFormSubmit = (date: IFormValues) => {
-        const project = {...date, id: Math.random().toString(), backlog: [], sprints: []}
+    const handleProjectFormSubmit = (data: IFormValues) => {
+        const project = {...data, id: Math.random().toString(), backlog: [], sprints: []}
         dispatch(addProject(project))
     }
 
     return (
-        <Container maxWidth="lg" style={{marginTop: '2rem'}}>
-            <ProjectForm onSubmit={handleProjectFormSubmit} type='project'/>
+        <Container maxWidth="lg">
+            <ProjectForm onSubmit={handleProjectFormSubmit} type={'sprint'}/>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <form onSubmit={handleSearchSubmit} style={{display: 'flex', marginTop: '1rem'}}>
@@ -46,10 +44,7 @@ function HomePage() {
                 </Grid>
                 {filteredProjects.map(project => (
                     <Grid item xs={12} sm={6} md={4} key={project.id}>
-                        <Link to={`/project/${project.id}`}>
-                            <ProjectCard description={project.description} title={project.title}
-                                         timestamp={project.timestamp} id={project.id}/>
-                        </Link>
+                        <ProjectCard project={project}/>
                     </Grid>
                 ))}
             </Grid>
